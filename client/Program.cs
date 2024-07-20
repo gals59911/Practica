25dae09f3d6ddbd2629a04398c1ff606c6059579
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2024-07-20 17:57:53 +0300

    rest_api

---
+++ client/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string a_token = "";
+            var client = new HttpClient();
+            try
+            {
+                client.BaseAddress = new Uri("https://localhost:7030/api/auth/token");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var tokenRequest = new TokenRequest
+                {
+                    username = "correct_username",
+                    password = "correct_password",
+                    grant_type = "password"
+                };
+                var content = new StringContent(JsonConvert.SerializeObject(tokenRequest), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("token", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var tokenResponse = await response.Content.ReadAsStringAsync();
+                    a_token = tokenResponse.Substring(17, 36);
+                    //Console.WriteLine(a_token);
+                    Console.WriteLine(tokenResponse);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: { response.ReasonPhrase}");
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Неверный логин или пароль");
+            }
+            Console.WriteLine("Выберите:\n 1) Справочник кодов для датчиков \n 2) Передача текущих данных \n 3) Передача архивных данных");
+            switch (int.Parse(Console.ReadLine()))
+            {
+
+                case 1:
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", a_token);
+                    HttpResponseMessage r1 = await client.GetAsync("https://localhost:7030/api/data/voc");
+                    if (r1.IsSuccessStatusCode)
+                    {
+
+                        string con = await r1.Content.ReadAsStringAsync();
+                        Console.WriteLine("Ответ сервера:");
+                        Console.WriteLine(con);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка при отправке запроса. Код ошибки: " + r1.StatusCode);
+                    }
+                    break;
+
+                case 2:
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", a_token);
+                    OnlineDataRequest requestData = new OnlineDataRequest
+                    {
+                        Rig = 1,
+                        Codes = new int[] { 6, 7 },
+                        Values = new double[] { 29.5, 30.5 }
+                    };
+
+                    string json = JsonConvert.SerializeObject(requestData);
+                    var c = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage r2 = await client.PostAsync("https://localhost:7030/api/data/online", c);
+                    if (r2.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Запрос успешно отправлен");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка при отправке запроса. Код ошибии: " + r2.StatusCode);
+                    }
+                    break;
+
+                case 3:
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", a_token);
+                    ArchiveDataRequest request_Data = new ArchiveDataRequest
+                    {
+                        Rig = 1,
+                        Codes = new int[] { 6, 7 },
+                        Data= new List<ArchiveDataEntry> { new ArchiveDataEntry {Date = 154562890800, Values=new double[] { 29.5, 1.5 } }, 
+                            new ArchiveDataEntry { Date = 1545628909000, Values = new double[] { 30.5, 1.5 } }, }
+                    };
+
+                    string jso = JsonConvert.SerializeObject(request_Data);
+                    var co = new StringContent(jso, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage r3 = await client.PostAsync("https://localhost:7030/api/data/archive", co);
+                    if (r3.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Запрос успешно отправлен");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка при отправке запроса. Код ошибии: " + r3.StatusCode);
+                    }
+                    break;
+
+
+
+
+                default:
+
+                    Console.WriteLine("Такой опции нет");
+                    break;
+
+            }
+
+
+
+
+        }
+        public class OnlineDataRequest
+        {
+            public int Rig { get; set; }
+            public int[] Codes { get; set; }
+            public double[] Values { get; set; }
+        }
+       
+
+        public class ArchiveDataEntry
+        {
+            public long Date { get; set; }
+            public double[] Values { get; set; }
+        }
+        public class ArchiveDataRequest
+        {
+            public int Rig { get; set; }
+            public int[] Codes { get; set; }
+            public List<ArchiveDataEntry> Data { get; set; }
+        }
+        public class TokenRequest
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+            public string grant_type { get; set; }
+        }
+    }
+}
---
+++ server_api/Controllers/AuthController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+
+
+namespace rest_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        
+        [HttpPost("token")]
+        public IActionResult Post([FromBody] TokenRequest request)
+        {
+            
+            if (request.grant_type != "password")
+            {
+                return BadRequest("Неверный тип авторизации.");
+            }
+
+            if (request.username == "correct_username" && request.password == "correct_password")
+            {
+                var token = Guid.NewGuid().ToString();
+
+                var responseObject = new
+                {
+                    access_token = $"{token}",
+                    token_type = "bearer",
+                    expires_in = 86399,
+                    refresh_token = Guid.NewGuid().ToString()
+                };
+                using (StreamWriter writer = new StreamWriter("a", false))
+                {
+                    writer.WriteLine(responseObject.access_token.ToString());
+                }
+
+                return Ok(responseObject);
+
+            }
+            else
+            {
+                return BadRequest("Неверный логин или пароль.");
+            }
+        }
+        [HttpGet("/api/data/voc")]
+        [CustomAuthorize]
+        public IActionResult GetVoc()
+        {
+            var sensorData = new[]
+            {
+            new { code = 6, name = "Вес на крюке", unit = "т" },
+            new { code = 7, name = "Момент на ключе", unit = "кН*м" }
+            };
+
+            return Ok(sensorData);
+        }
+        [HttpPost("/api/data/archive")]
+        [CustomAuthorize]
+        public IActionResult ArchiveData([FromBody] ArchiveDataRequest request)
+        {
+            if (request == null || request.Rig == 0 || request.Codes == null || request.Data == null)
+            {
+                return BadRequest("Invalid request parameters");
+            }
+            string dataString = JsonConvert.SerializeObject(request); 
+            string filePath = "archive_data.txt";
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine(dataString);
+            }
+
+            return Ok();
+        }
+        [HttpPost("/api/data/online")]
+        [CustomAuthorize]
+        public IActionResult OnlineData([FromBody] OnlineDataRequest request)
+        {
+            if (request == null || request.Rig == 0 || request.Codes == null || request.Values == null)
+            {
+                return BadRequest("Invalid request parameters");
+            }
+            string dataString = JsonConvert.SerializeObject(request); 
+            string filePath = "online_data.txt";
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+              sw.WriteLine(dataString);
+            }
+
+            return Ok();
+            }
+        }
+       
+}
+
+    public class CustomAuthorizeAttribute : Attribute, IAuthorizationFilter
+        {
+            public void OnAuthorization(AuthorizationFilterContext context)
+            {
+                var token = context.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+                if (string.IsNullOrEmpty(token) || IsValidToken(token))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+
+            private bool IsValidToken(string token)
+            {
+
+                string tok;
+                using (StreamReader reader = new StreamReader("a"))
+                {
+                    string line = reader.ReadToEnd();
+                    tok = line;
+
+                }
+
+                return token == tok;
+            }
+        }
+    
+    public class OnlineDataRequest
+    {
+        public int Rig { get; set; }
+        public int[] Codes { get; set; }
+        public double[] Values { get; set; }
+    }
+    public class ArchiveDataRequest
+    {
+        public int Rig { get; set; }
+        public int[] Codes { get; set; }
+        public List<ArchiveDataEntry> Data { get; set; }
+    }
+
+    public class ArchiveDataEntry
+    {
+        public long Date { get; set; }
+        public double[] Values { get; set; }
+    }
+    public class TokenRequest
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string grant_type { get; set; }
+    }
+
+
+
